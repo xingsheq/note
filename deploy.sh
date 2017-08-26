35b488d9991cd9d5a2d619d33f16101ca4387598
xingsheq <laoqi1999@163.com>
2017-08-26 17:38:45 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 chmod 600 id_rsa
 mv id_rsa ~/.ssh/id_rsa
-rsync -arHz --progress --delete -e 'ssh -p 2188' public/*  root@45.62.102.9:/home/travis/public/
+rsync -arHz --progress --delete -e 'ssh -p 28529' public/*  root@45.62.102.9:/home/travis/public/
